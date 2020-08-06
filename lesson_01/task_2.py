385ed9d9f2285127f230f3c76ad90a34e8187223
sab1e <antony.sobolev@gmail.com>
2020-08-06 19:36:28 +0500

    add task_2

--- lesson_01/task_2.py
+++ lesson_01/task_2.py
@@ -1,20 +1,36 @@
-"""
-Задание 2.	Дополнить следующую функцию недостающим кодом:
+import os
+
+
 def print_directory_contents(sPath):
+    """
+        Функция принимает имя каталога и распечатывает его содержимое
+        в виде «путь и имя файла», а также любые другие
+        файлы во вложенных каталогах.
 
-    Функция принимает имя каталога и распечатывает его содержимое
-    в виде «путь и имя файла», а также любые другие
-    файлы во вложенных каталогах.
+        Эта функция подобна os.walk. Использовать функцию os.walk
+        нельзя. Данная задача показывает ваше умение работать с
+        вложенными структурами.
+        заполните далее
 
-    Эта функция подобна os.walk. Использовать функцию os.walk
-    нельзя. Данная задача показывает ваше умение работать с
-    вложенными структурами.
-    заполните далее
+        Пример:
+        [
+        ('mainapp', 'admin.py'),
+        ('mainapp\\management\\commands', 'seed_db.py'),
+        ...
+        ]
+    """
 
-Пример:
-[
-('mainapp', 'admin.py'),
-('mainapp\\management\\commands', 'seed_db.py'),
-...
-]
-"""
+    if os.path.isfile(sPath):
+        return os.path.split(sPath)[1]
+    else:
+        directory_list = [sPath]
+        directory_list.extend([print_directory_contents(
+            os.path.join(sPath, element)) for element in os.listdir(sPath)])
+        return directory_list
+
+
+if __name__ == '__main__':
+    current_path = '/home/anton/projects/untitled/'
+    directory_list = print_directory_contents(current_path)
+    for i in directory_list:
+        print(i)
